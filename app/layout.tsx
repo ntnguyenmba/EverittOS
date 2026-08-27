6feec12100b375dc83bae3efe3a0908c9315e846
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-08-26 19:38:27 -0500

    Restore working root layout and keep visual styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,15 +78,14 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             <SessionGuard>
               <ActivityHeartbeat />
               <WorkspacePlanProvider>
-                <WorkspaceBootstrap>
-                  <SkipToMain />
-                  <SiteChrome />
-                  <ContractorStaticSections />
-                  {children}
-                  <MobileBottomNav />
-                  <AnalyticsGate />
-                  <CookieConsentBanner />
-                </WorkspaceBootstrap>
+                <WorkspaceBootstrap />
+                <SkipToMain />
+                <SiteChrome />
+                <ContractorStaticSections />
+                {children}
+                <MobileBottomNav />
+                <AnalyticsGate />
+                <CookieConsentBanner />
               </WorkspacePlanProvider>
             </SessionGuard>
           </ToastProvider>
