771f9742b8acbcb2098a54eaf93a5653afddbf00
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-08-26 19:35:53 -0500

    Fix root layout JSX nesting

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,7 +78,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             <SessionGuard>
               <ActivityHeartbeat />
               <WorkspacePlanProvider>
-                <WorkspaceBootstrap />
+                <WorkspaceBootstrap>
                   <SkipToMain />
                   <SiteChrome />
                   <ContractorStaticSections />
