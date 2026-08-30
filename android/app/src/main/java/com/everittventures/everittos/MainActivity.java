3a8f8748d926b5725a53f13515064efe08fb4c0e
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-08-30 06:46:27 -0500

    Register Android field cache plugin

--- android/app/src/main/java/com/everittventures/everittos/MainActivity.java
+++ android/app/src/main/java/com/everittventures/everittos/MainActivity.java
@@ -3,12 +3,14 @@ package com.everittventures.everittos;
 import android.os.Bundle;
 
 import com.everittventures.everittos.billing.EverittBillingPlugin;
+import com.everittventures.everittos.field.EverittFieldStorePlugin;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(EverittBillingPlugin.class);
+        registerPlugin(EverittFieldStorePlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
