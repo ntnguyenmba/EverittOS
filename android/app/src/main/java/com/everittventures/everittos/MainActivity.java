a09a5c1f41cb7ee6823b2c3300805eeb7fb8305b
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-08-30 07:02:07 -0500

    Register Android secure store plugin

--- android/app/src/main/java/com/everittventures/everittos/MainActivity.java
+++ android/app/src/main/java/com/everittventures/everittos/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.everittventures.everittos.billing.EverittBillingPlugin;
 import com.everittventures.everittos.field.EverittFieldStorePlugin;
+import com.everittventures.everittos.security.EverittSecureStorePlugin;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -11,6 +12,7 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(EverittBillingPlugin.class);
         registerPlugin(EverittFieldStorePlugin.class);
+        registerPlugin(EverittSecureStorePlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
