d739eb0bfe92cbad373f2c9ade610e56894b4f74
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-09-11 07:52:13 -0500

    Load central fixed header rules last

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,7 @@ import './viewport-lock.css';
 
 /* One centralized signed-in visual authority. Keep last. */
 import './control-breathing.css';
+import './signed-in-sticky-header.css';
 
 const manrope = Manrope({ subsets: ['latin', 'vietnamese'], weight: ['400', '500', '600', '700', '800'], variable: '--font-manrope', display: 'swap' });
 export const viewport: Viewport = { width: 'device-width', initialScale: 1, maximumScale: 1, viewportFit: 'cover', themeColor: '#243F53' };
